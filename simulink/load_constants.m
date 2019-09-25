986311070a5f0508dac0047bbfd27f27501d77b1
danielstumpp <dcs98@pitt.edu>
2019-09-24 21:00:53 -0400

    Revert "Revert "Revert "added simulink model"""
    This reverts commit b82ad8ae365fc0f4cf4696563acaf238baf37fc5.

--- simulink/load_constants.m
+++
@@ -1,9 +0,0 @@
-
-motor                                    % Load motor specs
-
-Cd = .51;                                % coefficient of drag
-diameter = 0.156337;                     % diameter of rocket in meters
-rocketMass =  15.51286 - totalWeight;    % mass of rocket in kg (not including motor)
-
-padLat = 40.2;                           %lattitude of pad
-padLong = 76.8;                          %longitude of pad
