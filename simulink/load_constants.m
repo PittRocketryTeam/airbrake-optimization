24ce25306b781d6e6882baa69871815da0e68618
danielstumpp <dcs98@pitt.edu>
2019-09-24 21:02:14 -0400

    added simulink model

---
+++ simulink/load_constants.m
@@ -0,0 +1,9 @@
+
+motor                                    % Load motor specs
+
+Cd = .51;                                % coefficient of drag
+diameter = 0.156337;                     % diameter of rocket in meters
+rocketMass =  15.51286 - totalWeight;    % mass of rocket in kg (not including motor)
+
+padLat = 40.2;                           %lattitude of pad
+padLong = 76.8;                          %longitude of pad
